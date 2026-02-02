c507ddc008eec9647ae6fd7eebd8a4bd0134deee
Ilya Kharyn <ilya.tt07@gmail.com>
2026-02-02 23:39:33 +0100

    update Python version requirement; add noqa comments

--- sample/sample/main.py
+++ sample/sample/main.py
@@ -17,8 +17,4 @@ import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample.settings')
 
-from bazis.core.app import app
-
-
-if __name__ == "__main__":
-    app.uvicorn_start()
+from bazis.core.app import app  # noqa: E401
